cd79ac4e08ed084d2c05a23b3707d015e31f0921
Anmoldeep <107442823+adsingh921@users.noreply.github.com>
2026-07-22 22:24:27 -0700

    Create Solution1.sql

---
+++ sql/sql_assignment_01/Solution1.sql
@@ -0,0 +1,15 @@
+--Anmoldeep Sandhu (7/22/26)
+
+CREATE TABLE Writer (
+	writer_id INT PRIMARY KEY,
+	writer_name VARCHAR(150)
+);
+
+CREATE TABLE Books (
+	book_id INT PRIMARY KEY,
+	title VARCHAR(255),
+	author INT,
+	publication INT,
+	page_count INT,
+	FOREIGN KEY (author) REFRENCES Writer(writer_id)
+);
