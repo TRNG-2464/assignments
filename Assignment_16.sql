b4f90d63d6f267bedfa615ad616afd7bd3e9adc0
betohhh707 <betohhh.ha@gmail.com>
2026-07-24 14:41:53 -0700

    2 Selects for doctor names

---
+++ Assignment_16.sql
@@ -0,0 +1,3 @@
+SELECT Doctor.first_name, Doctor.last_name FROM Doctor LEFT JOIN Appointment ON Doctor.doctor_id=Appointment.doctor_id WHERE Appointment.doctor_id IS NULL;
+SELECT Doctor.first_name, Doctor.last_name FROM Doctor LEFT JOIN Appointment ON Doctor.doctor_id=Appointment.doctor_id AND Appointment.status = 'Scheduled'
+WHERE Appointment.appointment_id IS NULL;
