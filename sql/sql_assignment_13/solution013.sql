3ae60005215656d99c6f2bc06d3c914415c58020
Aalok Zimmerman <98975330+azimmerman1245@users.noreply.github.com>
2026-07-24 16:29:28 -0500

    Add SQL query for players in football teams
    Query to select players with salaries above 200,000 in football.

---
+++ sql/sql_assignment_13/solution013.sql
@@ -0,0 +1,7 @@
+-- Solution
+
+-- A double join? Interesting Player -> Team -> Sport
+
+SELECT player_name 
+FROM Player INNER JOIN Team ON Player.team_id = Team.team_id INNER JOIN Sport ON Team.sport_id = Sport.sport_id
+WHERE Sport.sport_name = 'Foot Ball' AND Player.player_salary > 200000;
