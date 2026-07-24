dd6b3da7b2ff80c05a8bb1a970ee964c5d9a811c
Aalok Zimmerman <98975330+azimmerman1245@users.noreply.github.com>
2026-07-24 16:37:39 -0500

    Fix SQL query to select player names correctly

--- sql/sql_assignment_13/solution013.sql
+++ sql/sql_assignment_13/solution013.sql
@@ -2,6 +2,6 @@
 
 -- A double join? Interesting Player -> Team -> Sport
 
-SELECT player_name 
+SELECT Player.player_name 
 FROM Player INNER JOIN Team ON Player.team_id = Team.team_id INNER JOIN Sport ON Team.sport_id = Sport.sport_id
 WHERE Sport.sport_name = 'Foot Ball' AND Player.player_salary > 200000;
