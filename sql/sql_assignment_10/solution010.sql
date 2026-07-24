16d08d9f7dbd0a187ebf1676a6c637eb4a66400a
Aalok Zimmerman <98975330+azimmerman1245@users.noreply.github.com>
2026-07-24 15:58:20 -0500

    Fix SQL query syntax for author name condition

--- sql/sql_assignment_10/solution010.sql
+++ sql/sql_assignment_10/solution010.sql
@@ -2,4 +2,4 @@
 
 SELECT SUM(Books.page_count) AS TotalPageCount
 FROM Author INNER JOIN Books ON Author.author_id = Books.author
-WHERE Author.author_name = 'Stephen King'
+WHERE Author.author_name = 'Stephen King';
