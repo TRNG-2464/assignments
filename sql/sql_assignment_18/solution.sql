336fd76a65046b93d2284974e6ebf90a86d58d81
Carlos Rivera <61571282+carlosariv@users.noreply.github.com>
2026-07-26 00:31:41 -0500

    18 Stretch goals

--- sql/sql_assignment_18/solution.sql
+++ sql/sql_assignment_18/solution.sql
@@ -26,5 +26,44 @@ FROM Product
 ORDER BY margin DESC
 LIMIT 1;
 
--- SG 1
+-- TODO: Look into window functions
 
+-- SG 1
+CREATE VIEW v_salesperson_revenue AS
+SELECT sp.salesperson_id, sp.salesperson_name, SUM(p.unit_price * s.quantity) AS revenue, sp.region_id
+FROM Salesperson sp
+JOIN Sale s ON sp.salesperson_id = s.salesperson_id
+JOIN Product p ON s.product_id = p.product_id
+GROUP BY sp.salesperson_id;
+
+CREATE VIEW v_max_region_revenue AS
+SELECT r.region_id, MAX(revenue) AS max_revenue
+FROM Region r
+JOIN v_salesperson_revenue sr ON sr.region_id =  r.region_id
+GROUP BY r.region_id;
+
+SELECT r.region_name, sr.salesperson_name, mr.max_revenue
+FROM v_salesperson_revenue sr
+JOIN Region r ON sr.region_id = r.region_id
+JOIN v_max_region_revenue mr ON r.region_id = mr.region_id
+WHERE mr.max_revenue = sr.revenue;
+
+-- SG 2
+CREATE VIEW v_units_sold AS
+SELECT r.region_id, p.product_id, SUM(s.quantity) AS total_units
+FROM Region r
+JOIN Salesperson sp ON r.region_id = sp.region_id
+JOIN Sale s ON sp.salesperson_id = s.salesperson_id
+JOIN Product p ON s.product_id = p.product_id
+GROUP BY r.region_id, p.product_id;
+
+CREATE VIEW v_max_sold AS
+SELECT us.region_id, MAX(us.total_units) AS max_units
+FROM v_units_sold us
+GROUP BY us.region_id;
+
+SELECT us.region_id, p.product_name, ms.max_units
+FROM v_units_sold us
+JOIN Product p ON us.product_id = p.product_id
+JOIN v_max_sold ms ON us.region_id = ms.region_id
+WHERE us.total_units = ms.max_units;
