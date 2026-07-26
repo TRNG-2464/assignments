6205321e66c6268259d17898c5c2b6d9586d1cc8
Anmoldeep <107442823+adsingh921@users.noreply.github.com>
2026-07-26 16:17:23 -0700

    Create Solution7.sql

---
+++ sql/sql_assignment_07/Solution7.sql
@@ -0,0 +1,8 @@
+--Anmoldeep Solution 7
+
+SELECT * from Author;
+SELECT * from Books;
+
+
+INSERT INTO Books (isbn_13, title, author, publication, page_count)
+VALUES ('3063', 'Book of George', 1002, 1939, 200);
