9b1d60cd194d2b692ca36caef5d631f61f590e49
Michael Straus <mjstraus2304@gmail.com>
2026-07-29 11:16:25 -0700

    questions 4 and 5

---
+++ java/java_assignment_05/WordFrequencyCounter.java
@@ -0,0 +1,42 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WordFrequencyCounter {
+
+    public static void main(String[] args) {
+        countWords("The time I went to the store I bought a watermelon");
+        countWords("Every word here is unique");
+    }
+
+    public static void countWords(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        LinkedHashMap<String, Integer> wordCounts = new LinkedHashMap<>();
+
+        for (String word : words) {
+            word = word.toLowerCase();
+
+            if (wordCounts.containsKey(word)) {
+                wordCounts.put(word, wordCounts.get(word) + 1);
+            } else {
+                wordCounts.put(word, 1);
+            }
+        }
+
+        boolean first = true;
+
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+
+            if (!first) {
+                System.out.print(", ");
+            }
+
+            System.out.print(entry.getKey() + " - " + entry.getValue());
+
+            first = false;
+        }
+
+        System.out.println();
+    }
+}
