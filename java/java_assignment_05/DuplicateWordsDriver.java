73f8ea0cbc79c7e28a71c21209a1bed5e8fbc1d8
timmypass17 <69086587+timmypass17@users.noreply.github.com>
2026-07-30 12:02:59 -0700

    Complete java assignment 5

---
+++ java/java_assignment_05/DuplicateWordsDriver.java
@@ -0,0 +1,9 @@
+public class DuplicateWordsDriver {
+    public static void main(String[] args) {
+        DuplicateWords d = new DuplicateWords();
+        d.getWordCount("The time I went to the store I bought a watermelon");
+        d.getUniqueWords("The time I went to the store I bought a watermelon");
+        d.getSentenceStatistics("The time I went to the store I bought a watermelon");
+    }
+    
+}
