c38d860fab1eb80d2cf50c4cc3dfa50ae6058dfa
franceshogg <123121477+franceshogg@users.noreply.github.com>
2026-07-31 15:01:25 -0700

    Add files via upload

---
+++ java/java_assignment_07/Bird.java
@@ -0,0 +1,13 @@
+package exercise7;
+
+public class Bird extends Animal {
+    public Bird(String name, String color, String size, String noise) {
+        super(name, color, size, noise);
+    }
+    public void describe() {
+        super.describe();
+    }
+    public void makeSound() {
+        super.makeSound();
+    }
+}
---
+++ java/java_assignment_07/HorseStretch2.java
@@ -0,0 +1,13 @@
+package stretch2.exercise7;
+
+public class Horse extends Animal {
+    public Horse(String name, String color, String size, String noise) {
+        super(name, color, size, noise);
+    }
+    public void describe() {
+        super.describe();
+    }
+    public void makeSound() {
+        super.makeSound();
+    }
+}
