fe542a348b31212a7556ec97af6de36a7fdee379
franceshogg <123121477+franceshogg@users.noreply.github.com>
2026-07-31 15:12:38 -0700

    Add files via upload

---
+++ java/java_assignment_08/exercise8stretchgoal1/Main.java
@@ -0,0 +1,53 @@
+package exercise8stretch1.exercise8;
+
+public class Main {
+    public static void main (String[] args) {
+        try {
+            order("Laptop", -2, 10, 600, 600, true);
+        }
+        catch (InvalidQuantityException e) {
+            System.out.println("Order failed: " + e.getMessage());
+        }
+        catch (OutOfStockException e) {
+            System.out.println("Order failed: " + e.getMessage());
+        }
+        catch (PaymentExpiredException e) {
+            System.out.println("Order failed: " + e.getMessage());
+        }
+        catch (PaymentDeclinedException e) {
+            System.out.println("Order failed: " + e.getMessage());
+        }
+
+        try {
+            order("Laptop", 2, 10, 500, 600, true);
+        }
+        catch (InvalidQuantityException e) {
+            System.out.println("Invalid quantity: " + e.getMessage());
+        }
+        catch (OutOfStockException e) {
+            System.out.println("Stock error: " + e.getMessage());
+        }
+        catch (PaymentExpiredException e) {
+            System.out.println("Payment error: " + e.getMessage());
+        }
+        catch (PaymentDeclinedException e) {
+            System.out.println("Payment error: " + e.getMessage());
+        }
+    }
+    public static void order(String name, int quantity, int available_stock, double payment, double cost, boolean payment_expired) throws InvalidQuantityException, OutOfStockException, PaymentExpiredException, PaymentDeclinedException {
+        if (quantity <= 0) {
+            throw new InvalidQuantityException("Quantity must be greater than 0.", quantity);
+        }
+        else if (quantity > available_stock) {
+            throw new OutOfStockException("Quantity exceeds available stock.");
+        }
+        else if (payment_expired) {
+            throw new PaymentExpiredException("Your payment has expired.");
+        }
+        else if (payment < cost) {
+            throw new PaymentDeclinedException("You need to pay more.");
+        } else {
+            System.out.println("Order successful! " + quantity + " x " + name + " confirmed.");
+        }
+    }
+}
---
+++ java/java_assignment_08/exercise8stretchgoal1/PaymentExpiredException.java
@@ -0,0 +1,7 @@
+package exercise8stretch1.exercise8;
+
+public class PaymentExpiredException extends PaymentException {
+    public PaymentExpiredException(String message) {
+        super(message);
+    }
+}
