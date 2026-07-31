4385395982f6a15c6428c56c9c0845e34da0c99c
franceshogg <123121477+franceshogg@users.noreply.github.com>
2026-07-31 15:06:10 -0700

    Add files via upload

---
+++ java/java_assignment_07/exercise7stretchgoal2/BirdStretch2.java
@@ -0,0 +1,13 @@
+package stretch2.exercise7;
+
+public class Bird extends Animal {
+    public Bird(String name, String color, String size, String noise) {
+        super(name, color, size, noise);
+    }
+    public void describe() {
+        super.describe();
+    }
+    public void makeSound() {
+        super.makeSound();
+    }
+}
---
+++ java/java_assignment_07/exercise7stretchgoal2/PigStretch2.java
@@ -0,0 +1,13 @@
+package stretch2.exercise7;
+
+public class Pig extends Animal {
+    public Pig(String name, String color, String size, String noise) {
+        super(name, color, size, noise);
+    }
+    public void describe() {
+        super.describe();
+    }
+    public void makeSound() {
+        super.makeSound();
+    }
+}
